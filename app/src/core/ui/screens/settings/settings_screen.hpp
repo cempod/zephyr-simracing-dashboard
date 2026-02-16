29846639f061052fd2d159939e783faa38e71274
cempod <cempod@gmail.com>
2026-02-16 19:51:29 +0300

    Fix

--- app/src/core/ui/screens/settings/settings_screen.hpp
+++ app/src/core/ui/screens/settings/settings_screen.hpp
@@ -12,17 +12,4 @@ class SettingsScreen : public ScreenBase {
         static SettingsScreen* instance;
         
         virtual ~SettingsScreen() = default;
-
-        DECLARE_EVENT(speed, EV_SPEED, 0);
-        DECLARE_EVENT(gear, EV_GEAR, 0);
-        DECLARE_EVENT(shift, EV_SHIFT, 0);
-        DECLARE_EVENT(rpm, EV_RPM, 0);
-        DECLARE_EVENT(rpm_pct, EV_RPM_PCT, 0);
-        DECLARE_EVENT(handbrake, EV_HANDBRAKE, 0);
-        DECLARE_EVENT(esp, EV_ESP, -1);
-        DECLARE_EVENT(abs, EV_ABS, -1);
-        DECLARE_EVENT(turn_lights, EV_TURN, 0);
-        DECLARE_EVENT(beam, EV_BEAM, 0);
-        DECLARE_EVENT(fuel_pct, EV_FUEL_PCT, 0);
-        DECLARE_EVENT(fuel_alarm, EV_FUEL_ALARM, 0);
 };
